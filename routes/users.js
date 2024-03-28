d2e0af9884609dbee07162840790223f9bd1b86b
AnantMoorthy <moorthyanant@gmail.com>
2024-03-27 22:08:19 -0400

    changes made from theia lab environment

--- routes/users.js
+++ routes/users.js
@@ -26,12 +26,16 @@ let users = [
 // GET request: Retrieve all users
 router.get("/",(req,res)=>{
   // Copy the code here
+  res.send(users);
   res.send("Yet to be implemented")//This line is to be replaced with actual return value
 });
 
 // GET by specific ID request: Retrieve a single user with email ID
 router.get("/:email",(req,res)=>{
   // Copy the code here
+    const email = req.params.email;
+    let filtered_users = users.filter((user) => user.email === email);
+    res.send(filtered_users);
   res.send("Yet to be implemented")//This line is to be replaced with actual return value
 });
 
